70f1b7c2a89ee3db81d80046308ba052d7465557
Mere Daniel <mereugochukwudaniel@gmail.com>
2021-08-26 08:25:31 +0100

    Committed changes to my application

---
+++ Controllers/EditAlbumController.cs
@@ -0,0 +1,38 @@
+﻿using FeildenCleggBradley_Studios.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FeildenCleggBradley_Studios.Controllers
+{
+    public class EditAlbumController : Controller
+    {
+        public IActionResult Index()
+        {
+             
+            return View();
+        }
+        public async Task<IActionResult> EditAlbums()
+        {
+            var albums = new List<Album>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/users/");
+                var response = await client.GetAsync("1/albums");        
+                var result = await response.Content.ReadAsStringAsync();           
+                albums = JsonConvert.DeserializeObject<List<Album>>(result);
+            }
+            return View(albums);
+        } 
+        public async Task<bool> EditPost(string value)
+        {
+            if (value != null)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿
+
+
+function edit(e) {
+  
+    var cardtraget = e.target.closest("#cover");
+    var cardtraget1 = e.target.closest("#card");
+    var p = cardtraget.childNodes[3].childNodes[1];
+    p.removeAttribute('hidden');
+    cardtraget1.style.backgroundColor = 'green';
+    var d = cardtraget1.children[0].childNodes[0].parentElement;
+   
+    d.classList.add("onedit");
+ 
+    var title = cardtraget1.children[0].childNodes[0].data;
+    var inp = p.childNodes[1].childNodes[1][1];
+    inp.value = title;
+   
+}
+function update(e) {
+    debugger
+    e.preventDefault();
+    var newValue = e.target.parentNode.parentNode[1];
+    var title = document.getElementsByClassName("onedit");
+    title[0].innerHTML = newValue.value;
+    title[0].classList.remove("onedit");
+    var div = e.target.closest(".me");
+    div.hidden = true;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "EditPost?value="+newValue.value , true);
+    xhttp.send();
+
+   // title.innerText = newValue.value;
+   
+}
+
